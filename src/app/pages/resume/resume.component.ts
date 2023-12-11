ffd7d1561704d8c227cec0e8f8ba824168a78531
Jeremy Young <young.a.jeremy@gmail.com>
2023-12-10 23:04:32 -0700

    resume component kind of working

--- src/app/pages/resume/resume.component.ts
+++ src/app/pages/resume/resume.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @Component({
   selector: 'jay-portfolio-ng-resume',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PdfViewerModule],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css',
 })
-export class ResumeComponent {}
+export class ResumeComponent {
+  pdfSrc = "../../../assets/files/Resume-JeremyYoung2022.pdf"
+}
